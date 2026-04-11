e3fb9340a4b074a76e7cb6dbb54cd0fe725f1d52
Snehitha <namburisnehitha@gmail.com>
2026-04-11 17:39:32 +0000

    add UserService with all repository methods

--- service/user_service.go
+++ service/user_service.go
@@ -3,11 +3,35 @@ package service
 import "github.com/namburisnehitha/IssueTracker/domain"
 
 type UserService struct {
-	UserRepository domain.UserRepository
+	userRepository domain.UserRepository
 }
 
-func NewUserService(UserRepository domain.UserRepository) UserService {
+func NewUserService(userRepository domain.UserRepository) UserService {
 	return UserService{
-		UserRepository: UserRepository,
+		userRepository: userRepository,
 	}
+}
+
+func (u *UserService) GetById(id string) (domain.User, error) {
+	return u.userRepository.GetById(id)
+}
+
+func (u *UserService) GetByName(name string) (domain.User, error) {
+	return u.userRepository.GetByName(name)
+}
+
+func (u *UserService) GetByRole(role domain.Roles) ([]domain.User, error) {
+	return u.userRepository.GetByRole(role)
+}
+
+func (u *UserService) UpdateUser(user domain.User) error {
+	return u.userRepository.UpdateUser(user)
+}
+
+func (u *UserService) DeleteUser(user domain.User) error {
+	return u.userRepository.DeleteUser(user)
+}
+
+func (u *UserService) UserList() ([]domain.User, error) {
+	return u.userRepository.UserList()
 }
