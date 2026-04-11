dd3ead62aed33e73739e14f3a77cf7ce215ab22f
Snehitha <namburisnehitha@gmail.com>
2026-04-11 17:23:32 +0000

    add ActivityService with Repository methods

---
+++ service/activity_service.go
@@ -0,0 +1,35 @@
+package service
+
+import "github.com/namburisnehitha/IssueTracker/domain"
+
+type ActivityService struct {
+	activityRepository domain.ActivityRepository
+}
+
+func NewActivityService(activityRepository domain.ActivityRepository) ActivityService {
+	return ActivityService{
+		activityRepository: activityRepository,
+	}
+}
+
+func (a *ActivityService) CreateActivity(issueid string, userid string, description string, action domain.ActivityType) error {
+	activity := domain.NewActivity(issueid, userid, description, action)
+	return a.activityRepository.Save(activity)
+
+}
+
+func (a *ActivityService) GetByUserId(userid string) ([]domain.Activity, error) {
+	return a.activityRepository.GetByUserId(userid)
+}
+
+func (a *ActivityService) GetByIssueId(issueid string) ([]domain.Activity, error) {
+	return a.activityRepository.GetByIssueId(issueid)
+}
+
+func (a *ActivityService) GetByAction(action domain.ActivityType) ([]domain.Activity, error) {
+	return a.activityRepository.GetByAction(action)
+}
+
+func (a *ActivityService) ActivityList() ([]domain.Activity, error) {
+	return a.activityRepository.ActivityList()
+}
