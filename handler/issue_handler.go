400ad03d1603af7e9b8e1fb87bf037b9ab33dd30
Snehitha <namburisnehitha@gmail.com>
2026-04-20 07:05:06 +0000

    feat : add issue handler with CRUD end points

--- handler/issue_handler.go
+++ handler/issue_handler.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi/v5"
+	"github.com/namburisnehitha/IssueTracker/domain"
 	"github.com/namburisnehitha/IssueTracker/service"
 	"net/http"
 )
@@ -33,11 +35,112 @@ func (i *IssueHandler) CreateIssue(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = i.issueService.CreateIssue(ir.Id, ir.Title, ir.Description)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+}
+
+func (i *IssueHandler) GetById(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+	issue, err := i.issueService.GetById(id)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(issue)
+}
+
+func (i *IssueHandler) GetByTitle(w http.ResponseWriter, r *http.Request) {
+
+	title := r.URL.Query().Get("title")
+	issue, err := i.issueService.GetByTitle(title)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(issue)
+}
+
+func (i *IssueHandler) GetByStatus(w http.ResponseWriter, r *http.Request) {
+
+	status := r.URL.Query().Get("status")
+	issue, err := i.issueService.GetByStatus(domain.IssueStatus(status))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(issue)
+}
+
+func (i *IssueHandler) UpdateIssue(w http.ResponseWriter, r *http.Request) {
+
+	var ir CreateIssueRequest
+
+	id := chi.URLParam(r, "id")
+	issue, err := i.issueService.GetById(id)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = json.NewDecoder(r.Body).Decode(&ir)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	issue.Title = ir.Title
+	issue.Description = ir.Description
+	err = i.issueService.UpdateIssue(issue)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusAccepted)
 
+}
+
+func (i *IssueHandler) DeleteIssue(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+	issue, err := i.issueService.GetById(id)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = i.issueService.DeleteIssue(issue)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+}
+
+func (i *IssueHandler) ListIssues(w http.ResponseWriter, r *http.Request) {
+
+	issues, err := i.issueService.ListIssues()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(issues)
 }
