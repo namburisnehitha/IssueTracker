65944f1ad0ce6b355536dbb08b9115af58682e1a
Snehitha <namburisnehitha@gmail.com>
2026-04-25 18:05:28 +0000

    feat: implement Save method on PostgresUsersRepository

--- internal/postgres/user_repo.go
+++ internal/postgres/user_repo.go
@@ -1,17 +1,23 @@
-CREATE TABLE users (
-    id  	 			TEXT 		PRIMARY KEY,
-    user_name  	 		TEXT 		NOT NULL,
-	user_role 	 		TEXT 		NOT NULL,
-	joined_at 			TIMESTAMP	NOT NULL,
-	changed_role_at		TIMESTAMP   
-);
+package postgres
 
-type PostgresUsersRepository struct{
-	db     *sql.DB
+import (
+	"database/sql"
+
+	"github.com/namburisnehitha/IssueTracker/domain"
+)
+
+type PostgresUsersRepository struct {
+	db *sql.DB
 }
 
-func NewPostgresUserRepository(db *sql.DB)*PostgresUsersRepository{
+func NewPostgresUserRepository(db *sql.DB) *PostgresUsersRepository {
 	return &PostgresUsersRepository{
 		db: db,
 	}
+}
+
+func (ur *PostgresUsersRepository) Save(user domain.User) error {
+	query := `INSERT INTO users (id,user_name,user_role,joined_at,changed_role_at) VALUES ($1, $2,$3,$4,$5)`
+	_, err := ur.db.Exec(query, user.Id, user.Name, user.Role, user.JoinedAt, user.ChangedRoleAt)
+	return err
 }
