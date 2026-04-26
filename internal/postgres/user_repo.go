89cfa46ab1d973f524e5b773a2cc677011cf7a1d
Snehitha <namburisnehitha@gmail.com>
2026-04-26 14:40:26 +0000

    feat: implement GetById method on PostgresUsersRepository

--- internal/postgres/user_repo.go
+++ internal/postgres/user_repo.go
@@ -2,7 +2,6 @@ package postgres
 
 import (
 	"database/sql"
-
 	"github.com/namburisnehitha/IssueTracker/domain"
 )
 
@@ -20,4 +19,11 @@ func (ur *PostgresUsersRepository) Save(user domain.User) error {
 	query := `INSERT INTO users (id,user_name,user_role,joined_at,changed_role_at) VALUES ($1, $2,$3,$4,$5)`
 	_, err := ur.db.Exec(query, user.Id, user.Name, user.Role, user.JoinedAt, user.ChangedRoleAt)
 	return err
+}
+
+func (ur *PostgresUsersRepository) GetById(id string) (domain.User, error) {
+	var user domain.User
+	query := `SELECT id,user_name,user_role,joined_at,changed_role_at FROM users WHERE id = $1`
+	err := ur.db.QueryRow(query, id).Scan(&user.Id, &user.Name, &user.Role, &user.JoinedAt, &user.ChangedRoleAt)
+	return user, err
 }
