5fd75d7f62c0834156dcdae7fbde128bbfc75ba1
Snehitha <namburisnehitha@gmail.com>
2026-04-26 18:33:16 +0000

    feat: verify PostgresUsersRepository Save with manual main.go test

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"github.com/namburisnehitha/IssueTracker/domain"
+	"github.com/namburisnehitha/IssueTracker/internal/postgres"
+)
+
+func main() {
+	db, err := postgres.NewDB("postgres://postgres:darealsql@localhost:5432/issuetracker?sslmode=disable")
+	if err != nil {
+		return
+	}
+	userrepo := postgres.NewPostgresUserRepository(db)
+	user, err := domain.NewUser("sneh", "id")
+	if err != nil {
+		return
+	}
+
+	err = userrepo.Save(user)
+	if err != nil {
+		fmt.Println("Save error:", err)
+	} else {
+		fmt.Println("Saved successfully")
+	}
+}
