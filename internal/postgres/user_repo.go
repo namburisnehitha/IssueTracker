df44f3ef8d841fcedd722ea45a438cb500c97690
Snehitha <namburisnehitha@gmail.com>
2026-04-26 16:38:11 +0000

    feat: implement GetByName method on PostgresUsersRepository

--- internal/postgres/user_repo.go
+++ internal/postgres/user_repo.go
@@ -25,5 +25,25 @@ func (ur *PostgresUsersRepository) GetById(id string) (domain.User, error) {
 	var user domain.User
 	query := `SELECT id,user_name,user_role,joined_at,changed_role_at FROM users WHERE id = $1`
 	err := ur.db.QueryRow(query, id).Scan(&user.Id, &user.Name, &user.Role, &user.JoinedAt, &user.ChangedRoleAt)
+
 	return user, err
+}
+
+func (ur *PostgresUsersRepository) GetByName(name string) ([]domain.User, error) {
+	var users []domain.User
+	query := `SELECT id,user_name,user_role,joined_at,changed_role_at FROM users WHERE user_name =$1`
+	rows, err := ur.db.Query(query, name)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u domain.User
+		err = rows.Scan(&u.Id, &u.Name, &u.Role, &u.JoinedAt, &u.ChangedRoleAt)
+		if err != nil {
+			return users, err
+		}
+		users = append(users, u)
+	}
+	return users, err
 }
