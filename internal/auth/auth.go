15cf7609737ff28716ebcc47cf3ad97672e7f380
Snehitha <namburisnehitha@gmail.com>
2026-05-08 00:51:51 +0000

    feat: add generate token abd validate token func

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -19,4 +23,39 @@ func CheckPasswordHash(password, hashedpassword string) bool {
 
 	err := bcrypt.CompareHashAndPassword([]byte(hashedpassword), []byte(password))
 	return err == nil
+}
+
+func GenerateToken(userID string) (string, error) {
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte("9q2CLgDY"))
+
+}
+
+func ReturnAsToken(token *jwt.Token) (interface{}, error) {
+	return []byte("9q2CLgDY"), nil
+}
+
+func ValidateToken(tokenString string) (string, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.MapClaims{}, ReturnAsToken)
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(*jwt.MapClaims)
+
+	if !ok || !token.Valid {
+		return "", errors.New("invalid token")
+	}
+
+	userID := (*claims)["user_id"].(string)
+
+	return userID, err
 }
