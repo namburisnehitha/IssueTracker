cb598cd7f2544808ed43e8ab9f57f70c3f157c86
Snehitha <namburisnehitha@gmail.com>
2026-05-15 08:38:09 +0000

    change UUID to TEXT

--- migrations/006_add_label.sql
+++ migrations/006_add_label.sql
@@ -1,5 +1,5 @@
 CREATE TABLE issue_labels (
-    issue_id UUID REFERENCES issues(id) ON DELETE CASCADE,
-    label_id UUID REFERENCES labels(id) ON DELETE CASCADE,
+    issue_id TEXT REFERENCES issues(id) ON DELETE CASCADE,
+    label_id TEXT REFERENCES labels(id) ON DELETE CASCADE,
     PRIMARY KEY (issue_id, label_id)
 );
