25def14d01aeaac44a4b329c01ef25ae209facfe
Snehitha <namburisnehitha@gmail.com>
2026-05-15 07:43:38 +0000

    refactor: change the table name to issue_labels

--- migrations/006_add_label.sql
+++ migrations/006_add_label.sql
@@ -1,4 +1,4 @@
-CREATE TABLE add_labels (
+CREATE TABLE issue_labels (
     issue_id UUID REFERENCES issues(id) ON DELETE CASCADE,
     label_id UUID REFERENCES labels(id) ON DELETE CASCADE,
     PRIMARY KEY (issue_id, label_id)
